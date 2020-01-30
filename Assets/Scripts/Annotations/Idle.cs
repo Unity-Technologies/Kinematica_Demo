dceca7d958e0282afbb18d9198b3a53e80390e55
Michael Buttner <michaelbu@unity3d.com>
2020-01-30 15:34:42 -0500

    Updated traits to reflect latest changes in the package

--- Assets/Scripts/Annotations/Idle.cs
+++ Assets/Scripts/Annotations/Idle.cs
@@ -1,3 +1,6 @@
+using Unity.Kinematica;
+
+[Trait]
 public struct Idle
 {
     public static Idle Default => new Idle();
--- Assets/Scripts/Annotations/Locomotion.cs
+++ Assets/Scripts/Annotations/Locomotion.cs
@@ -1,4 +1,6 @@
+using Unity.Kinematica;
 
+[Trait]
 public struct Locomotion
 {
     public static Locomotion Default => new Locomotion();
